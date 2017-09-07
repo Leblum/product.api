4564e7e734ca1bae9d512941a07de250097846b5
Dave Brown <dave@leblum.com>
2017-09-06 20:18:01 -0400

    turning down the ecosystem scaling

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,7 +9,7 @@ module.exports = {
     {
       name: 'prdct.api',
       script: './dist/server.js',
-      instances: 3,
+      instances: 1,
       max_memory_restart: '500M',
       max_restarts: 3,
       restart_delay: 3000,
